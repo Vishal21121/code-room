82ae4ba5fc9e8720f92886c3f840220bfad0c906
Vishal21121 <vs84077000@gmail.com>
2023-09-20 19:26:42 +0530

    added whiteboard elements persistance code

--- client/src/components/Whiteboard.jsx
+++ client/src/components/Whiteboard.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Excalidraw } from "@excalidraw/excalidraw";
 
 const Whiteboard = () => {
+    const [excalidrawApi, setExcalidrawApi] = useState(null)
+
+    const handlePointerUpdate = () => {
+        if (excalidrawApi) {
+            const elements = excalidrawApi.getSceneElements()
+            if (elements.length > 0) {
+                localStorage.setItem("Elements", JSON.stringify(elements))
+            }
+        }
+    };
+
+    useEffect(() => {
+        if (!excalidrawApi) {
+            return;
+        }
+        excalidrawApi.readyPromise.then((api) => {
+            console.log({ api });
+            const storedElements = JSON.parse(localStorage.getItem("Elements"));
+            console.log({ storedElements });
+            if (storedElements) {
+                setTimeout(() => {
+                    api.updateScene({ elements: storedElements });
+                }, 0)
+                console.log("updated");
+            }
+        });
+    }, [excalidrawApi])
     return (
         <div className='h-full w-full'>
-            <Excalidraw />
+            <Excalidraw
+                ref={api => setExcalidrawApi(api)}
+                onPointerUpdate={handlePointerUpdate}
+            >
+            </Excalidraw>
         </div>
     )
 }
