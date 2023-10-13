3b387f8dc26ff0ad22158b748863609f52d2cc89
Vishal21121 <vs84077000@gmail.com>
2023-10-13 17:45:48 +0530

    added: fetch message route and added: check roomId functionality in send-message controller

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -1,10 +1,20 @@
 import { Message } from "../models/message.models.js";
 import { Room } from "../models/room.models.js"
+import mongoose from "mongoose";
 
 
 export const sendMessage = async (req, res) => {
     const { message, roomId, username } = req.body
     try {
+        if (!mongoose.Types.ObjectId.isValid(roomId)) {
+            return res.status(400).json({
+                status: "failure",
+                data: {
+                    statusCode: 400,
+                    message: "Enter correct room id"
+                }
+            });
+        }
         const room = await Room.findById(roomId)
         if (!room) {
             return res.status(404).json({
@@ -53,4 +63,55 @@ export const sendMessage = async (req, res) => {
             }
         })
     }
+}
+
+export const fetchMessages = async (req, res) => {
+    const { roomId } = req.body
+    if (!roomId) {
+        return res.status(400).json({
+            status: "failure",
+            data: {
+                statusCode: 400,
+                message: "roomId not provided"
+            }
+        })
+    }
+    try {
+        if (!mongoose.Types.ObjectId.isValid(roomId)) {
+            return res.status(400).json({
+                status: "failure",
+                data: {
+                    statusCode: 400,
+                    message: "Enter correct room id"
+                }
+            });
+        }
+        const room = await Room.findById(roomId)
+        if (!room) {
+            return res.status(404).json({
+                status: "failure",
+                data: {
+                    statusCode: 404,
+                    message: "no room present with this roomId"
+                }
+            })
+        }
+        const messages = await Message.find({ roomId: roomId })
+        res.status(200).json({
+            status: "success",
+            data: {
+                statusCode: 200,
+                value: messages
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({
+            status: "failure",
+            data: {
+                statusCode: 500,
+                message: error.message || "Internal server error"
+            }
+        })
+    }
+
 }
--- server/routes/room-features.routes.js
+++ server/routes/room-features.routes.js
@@ -2,7 +2,7 @@ import express from "express"
 import { chatBot } from "../controllers/chat-bot.controllers.js"
 import { verifyJWT } from "../middleware/auth.middleware.js"
 import { addBoard, updateBoard, getBoardContent } from "../controllers/board.controllers.js"
-import { sendMessage } from "../controllers/message.controller.js"
+import { sendMessage, fetchMessages } from "../controllers/message.controller.js"
 import { validation } from "../middleware/validate.middlewares.js"
 import { messageValidator } from "../validators/message.validator.js"
 
@@ -13,5 +13,7 @@ router.route("/update-board").patch(verifyJWT, updateBoard)
 router.route("/add-board").post(verifyJWT, addBoard)
 router.route("/get-content").post(verifyJWT, getBoardContent)
 router.route("/send-message").post(verifyJWT, messageValidator(), validation, sendMessage)
+router.route("/get-message").post(verifyJWT, fetchMessages)
+
 
 export default router
