d8e0448738f1d315c84eb077fb7b1e72b8357475
Vishal21121 <vs84077000@gmail.com>
2023-10-14 18:15:26 +0530

    added: markdown messaging functionality in UserMessage.jsx

--- client/src/components/UserMessage.jsx
+++ client/src/components/UserMessage.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Avatar from 'react-avatar';
+import ReactMarkdown from "react-markdown"
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 
 const UserMessage = ({ username, message }) => {
@@ -10,7 +13,33 @@ const UserMessage = ({ username, message }) => {
                 <Avatar name={username} size={40} round="20px" />
                 <div className='flex flex-col'>
                     <p className='text-white'>{username}</p>
-                    <pre className='text-gray-300'>{message}</pre>
+                    <ReactMarkdown
+                        className='text-gray-300 w-fit markdown'
+                        children={message}
+                        components={{
+                            code({ node, inline, className, children, ...props }) {
+                                const match = /language-(\w+)/.exec(className || '')
+                                return !inline && match ? (
+                                    <>
+                                        <SyntaxHighlighter
+                                            {...props}
+                                            children={String(children).replace(/\n$/, '')}
+                                            style={dracula}
+                                            language={match[1]}
+                                            customStyle={{
+                                                padding: "25px"
+                                            }}
+                                            wrapLongLines="true"
+                                        />
+                                    </>
+                                ) : (
+                                    <code {...props} className={className}>
+                                        {children}
+                                    </code>
+                                )
+                            }
+                        }}
+                    />
                 </div>
             </div>
         </div>
