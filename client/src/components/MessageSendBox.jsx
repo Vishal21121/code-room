3fa86fbe1dcdc4c2129a66225270db37b2a12671
Vishal21121 <vs84077000@gmail.com>
2023-10-18 21:04:37 +0530

    added images paste functionlity

--- client/src/components/MessageSendBox.jsx
+++ client/src/components/MessageSendBox.jsx
@@ -68,8 +68,8 @@ const MessageSendBox = () => {
         }
     }
 
-    const handleUpload = async (e) => {
-        const response = await storage.createFile(import.meta.env.VITE_BUCKET_ID, ID.unique(), e.target.files[0])
+    const handleUpload = async (data) => {
+        const response = await storage.createFile(import.meta.env.VITE_BUCKET_ID, ID.unique(), data)
         let messageId = response.$id
         const filelink = storage.getFileView(import.meta.env.VITE_BUCKET_ID, messageId)
         const responseSubmit = await handleSubmit("media", filelink.href, true)
@@ -77,11 +77,16 @@ const MessageSendBox = () => {
         socketio.emit(ACTIONS.MESSAGE_SEND, { roomId, value: dataSubmitted.data.value })
     }
 
+    const handlePaste = async (e) => {
+        console.log(e.clipboardData.files[0]);
+        await handleUpload(e.clipboardData.files[0])
+    }
+
     return (
         <div className='absolute bottom-0 flex items-center bg-[#282a36] w-1/2 mx-auto p-2 rounded-lg mb-10 z-10'>
             <label htmlFor="file"><HiPlusCircle size={30} className='cursor-pointer text-gray-400' name='file' /></label>
-            <input type="file" name="" id="file" className='hidden' accept='image/*' onChange={handleUpload} />
-            <textarea type="text" className='box-border w-11/12 h-10  max-h-52 text-white outline-none bg-[#282a36] rounded-lg mr-2 z-30 resize-none p-2 overflow-auto' placeholder='Send a message' spellCheck="false" onChange={(e) => setMessage(e.target.value)} value={message} onKeyUp={handleKeyUp} onKeyDown={handleKeyDown} >
+            <input type="file" name="" id="file" className='hidden' accept='image/*' onChange={(e) => handleUpload(e.target.files[0])} />
+            <textarea type="text" className='box-border w-11/12 h-10  max-h-52 text-white outline-none bg-[#282a36] rounded-lg mr-2 z-30 resize-none p-2 overflow-auto' placeholder='Send a message' spellCheck="false" onChange={(e) => setMessage(e.target.value)} value={message} onKeyUp={handleKeyUp} onKeyDown={handleKeyDown} onPaste={handlePaste} >
             </textarea>
             <MdSend className='text-gray-500 my-auto cursor-pointer mr-4 hover:text-white' size={24} onClick={handleSubmit} />
         </div>
