86b25025f2969aaba8c12ed31083ecc0ab000c6a
Vishal21121 <vs84077000@gmail.com>
2023-10-20 00:14:47 +0530

    added ui in the sidebar component of chat-bot

--- client/src/components/ChatBotSidebar.jsx
+++ client/src/components/ChatBotSidebar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { RxPlus } from "react-icons/rx";
+import SidebarMessage from './SidebarMessage';
 
 const ChatBotSidebar = () => {
     return (
-        <div className='w-[20%] bg-gray-950'>
-
+        <div className='w-[20%] bg-gray-950 p-2 flex flex-col items-center gap-8'>
+            <div className='border w-[90%] p-2 rounded-md  ring-2 ring-gray-400 mt-2 text-left flex gap-2 items-center cursor-pointer hover:ring-white'>
+                <RxPlus size={20} className='text-gray-400' />
+                <p className='text-gray-300'>New Chat</p>
+            </div>
+            <div className='w-full h-full flex flex-col gap-4 items-center'>
+                <SidebarMessage />
+            </div>
         </div>
     )
 }
---
+++ client/src/components/SidebarMessage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { PiChatBold } from "react-icons/pi";
+
+const SidebarMessage = () => {
+    return (
+        <div className='w-[95%] ring-2 ring-gray-300 hover:ring-white p-4 rounded-lg bg-gray-900 flex gap-2 cursor-pointer'>
+            <PiChatBold size={20} className='text-gray-300' />
+            <p className='w-full whitespace-nowrap overflow-hidden text-ellipsis text-gray-300'>write a program to add two numbers in java</p>
+        </div>
+    )
+}
+
+export default SidebarMessage
