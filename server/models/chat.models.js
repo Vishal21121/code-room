dfae8cf1ce1b06faebd2705c453f76d1b27095ee
Vishal21121 <vs84077000@gmail.com>
2023-10-20 22:22:24 +0530

    added: model for chat bot chats

---
+++ server/models/chat.models.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose"
+import { AvailableSenderTypes } from "../util/constants.js"
+
+const chatContainerSchema = mongoose.Schema({
+    name: {
+        type: String
+    },
+    roomId: {
+        type: String
+    }
+})
+
+const chatSchema = mongoose.Schema({
+    roomId: {
+        type: String
+    },
+    chatContainerId: {
+        type: String
+    },
+    content: {
+        type: String
+    },
+    senderType: {
+        type: String,
+        enum: AvailableSenderTypes
+    },
+})
+
+export const Chat = mongoose.model("Chat", chatContainerSchema)
+export const ChatContainer = mongoose.model("ChatContainer", chatContainerSchema)
--- server/util/constants.js
+++ server/util/constants.js
@@ -3,4 +3,10 @@ export const MessageTypeEnum = {
     MEDIA: "media"
 }
 
+export const SenderTypeEnum = {
+    BOT: "bot",
+    USER: "user"
+}
+
 export const AvailableMessageTypes = Object.values(MessageTypeEnum)
+export const AvailableSenderTypes = Object.values(SenderTypeEnum)
