0227576bf1b9eaa7b301901733cf1bf37b1339fa
Vishal21121 <vs84077000@gmail.com>
2024-05-05 12:52:45 +0530

    refactor: Clean up Terminal component code

--- client/src/components/Code-Editor/Terminal.jsx
+++ client/src/components/Code-Editor/Terminal.jsx
@@ -1,91 +1,90 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { X } from 'react-feather';
-import { useSelector } from 'react-redux';
+import React, { useState, useRef, useEffect } from "react";
+import { X } from "react-feather";
+import { useSelector } from "react-redux";
 import { HiOutlineXCircle } from "react-icons/hi2";
-import { Terminal as xtermTerminal } from "xterm"
-import 'xterm/css/xterm.css';
-import { WebLinksAddon } from '@xterm/addon-web-links';
-import { FitAddon } from 'xterm-addon-fit';
-
-
-
+import { Terminal as xtermTerminal } from "xterm";
+import "xterm/css/xterm.css";
+import { WebLinksAddon } from "@xterm/addon-web-links";
+import { FitAddon } from "xterm-addon-fit";
 
 const Terminal = ({ output }) => {
-  const problems = useSelector((state) => state.problems.problems)
-  const [outputShow, setOutputShow] = useState(true)
-  const terminalEl = useRef(null)
-  const terminalInstance = useRef(null)
-  const socketio = useSelector((state) => state.socket.socket)
-  const terminalStarted = useRef(false)
+  const problems = useSelector((state) => state.problems.problems);
+  const [outputShow, setOutputShow] = useState(true);
+  const terminalEl = useRef(null);
+  const terminalInstance = useRef(null);
+  const socketio = useSelector((state) => state.socket.socket);
+  const terminalStarted = useRef(false);
   const fitAddon = new FitAddon();
 
   const closeTerminal = (e) => {
-    e.preventDefault()
-    let classList = document.getElementById("terminal").classList
-    classList.add("invisible")
-  }
+    e.preventDefault();
+    let classList = document.getElementById("terminal").classList;
+    classList.add("invisible");
+  };
 
   const terminalSetup = () => {
     terminalInstance.current = new xtermTerminal({
       // convertEol: true,
       theme: {
-        background: "#161a2a"
+        background: "#161a2a",
       },
     });
     terminalInstance.current.loadAddon(new WebLinksAddon());
     terminalInstance.current.loadAddon(fitAddon);
     console.log({ terminalInstance: terminalInstance.current });
-    terminalInstance.current.open(terminalEl.current)
+    terminalInstance.current.open(terminalEl.current);
     const resizeObserver = new ResizeObserver(() => {
       fitAddon.fit();
     });
-    resizeObserver.observe(document.querySelector('.terminal'));
-  }
+    resizeObserver.observe(document.querySelector(".terminal"));
+  };
 
   useEffect(() => {
-    let onKeyHandler
-    if(!terminalStarted.current){
-      terminalSetup()
-      console.log("rendered")
-      terminalStarted.current = true
-      onKeyHandler = terminalInstance.current.onKey((e)=>{
-        console.log("first",e.key)
-        if (e.key === '\r') {
-          terminalInstance.current.write('\n');
-        }else{
-          terminalInstance.current.write(e.key)
+    let onKeyHandler;
+    if (!terminalStarted.current) {
+      terminalSetup();
+      console.log("rendered");
+      terminalStarted.current = true;
+      onKeyHandler = terminalInstance.current.onKey((e) => {
+        console.log("first", e.key);
+        if (e.key === "\r") {
+          terminalInstance.current.write("\n");
+        } else {
+          terminalInstance.current.write(e.key);
         }
-      })
-      window.addEventListener('resize', () => {
+      });
+      window.addEventListener("resize", () => {
         fitAddon.fit();
       });
     }
-    terminalInstance.current.focus()
+    terminalInstance.current.focus();
 
-    terminalInstance.current.onData((data)=>{
-      socketio?.emit("data",data)
-    })
-    
+    terminalInstance.current.onData((data) => {
+      socketio?.emit("data", data);
+    });
+
     socketio?.on("data", (data) => {
       if (terminalInstance.current) {
         let decoder = new TextDecoder();
         let encodedData = decoder.decode(new Uint8Array(data));
         terminalInstance.current.write(encodedData);
-        console.log("encodedData", encodedData)
+        console.log("encodedData", encodedData);
       } else {
-        console.error('Terminal instance is not initialized');
+        console.error("Terminal instance is not initialized");
       }
-    })
-    return ()=>{
-      onKeyHandler?.dispose()
-    }
-  }, [socketio])
-
+    });
+    return () => {
+      onKeyHandler?.dispose();
+    };
+  }, [socketio]);
 
   return (
-    <div className='bg-[#161a2a] pl-4 relative bottom-[280px] left-[365px] flex flex-col w-1/2 z-50 h-[40vh] terminal' id='terminal' ref={terminalEl}>
-    </div>
-  )
-}
+    <div
+      className="bg-[#161a2a] pl-4 relative bottom-[280px] left-[365px] flex flex-col w-1/2 z-50 h-[40vh] terminal"
+      id="terminal"
+      ref={terminalEl}
+    ></div>
+  );
+};
 
-export default Terminal
+export default Terminal;
--- server/index.js
+++ server/index.js
@@ -1,167 +1,167 @@
-import express from "express"
-import connectToMongoDb from "./db/dbConnect.js"
-import dotenv from "dotenv"
-import userRouter from "./routes/user.routes.js"
-import roomFeaturesRouter from "./routes/room-features.routes.js"
-import roomRouter from "./routes/room.routes.js"
-import { Server } from "socket.io"
-import http from "http"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-import ACTIONS from "./util/Actions.js"
-import path from "path"
+import express from "express";
+import connectToMongoDb from "./db/dbConnect.js";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.routes.js";
+import roomFeaturesRouter from "./routes/room-features.routes.js";
+import roomRouter from "./routes/room.routes.js";
+import { Server } from "socket.io";
+import http from "http";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import ACTIONS from "./util/Actions.js";
+import path from "path";
 import { fileURLToPath } from "url";
 import Docker from "dockerode";
-import { WriteStream } from "fs"
-
+import { WriteStream } from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const app = express()
+const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 dotenv.config({
-    path: "./.env"
-})
+  path: "./.env",
+});
 
-connectToMongoDb()
+connectToMongoDb();
 
 const corsOptions = {
-    origin: ['http://localhost:5173', process.env.ALLOWED_ORIGIN],
-    credentials: true,
-    optionsSuccessStatus: 200
-}
+  origin: ["http://localhost:5173", process.env.ALLOWED_ORIGIN],
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
 
-app.use(cors(corsOptions))
-app.use(express.static(path.join(__dirname, '../client/dist')))
-app.use(express.json())
-app.use(cookieParser())
+app.use(cors(corsOptions));
+app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.json());
+app.use(cookieParser());
 
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/room-features", roomFeaturesRouter)
-app.use("/api/v1/room/", roomRouter)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/room-features", roomFeaturesRouter);
+app.use("/api/v1/room/", roomRouter);
 app.use("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client", 'dist', 'index.html'))
-})
+  res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+});
 
 // TODO: use database for this
 const userSocketMap = {};
 function getAllConnectedClients(roomId) {
-    // io.sockets.adapter.rooms.get(roomId) will return Map and here we are converting it to array 
-    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
-        (socketId) => {
-            return {
-                socketId,
-                username: userSocketMap[socketId],
-            };
-        }
-    );
+  // io.sockets.adapter.rooms.get(roomId) will return Map and here we are converting it to array
+  return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+    (socketId) => {
+      return {
+        socketId,
+        username: userSocketMap[socketId],
+      };
+    }
+  );
 }
 
 // TODO: NOT A GOOD WAY
-let containerInfo = {} // storing user's container information
+let containerInfo = {}; // storing user's container information
 
-
-const spinContainer = async(socket,socketId)=>{
-    const docker = new Docker();
-    try {
-      docker
-        .createContainer({
-          Image: "node:alpine3.19",
-          AttachStdin: true,
-          AttachStdout: true,
-          AttachStderr: true,
-          Tty: true,
-          Cmd: ["sh", "-c", "tail -f /dev/null"],
-          OpenStdin: true,
-        })
-        .then(function (container) {
-          containerInfo[socketId] = { container, id: container.id };
-          container.start(async function (data) {
-            console.log("data", data);
-            const exec = await container.exec({
-              Cmd: ["/bin/sh"],
-              AttachStderr: true,
-              AttachStdout: true,
-              AttachStdin: true,
-              Tty: true,
+const spinContainer = async (socket, socketId) => {
+  const docker = new Docker();
+  try {
+    docker
+      .createContainer({
+        Image: "node:alpine3.19",
+        AttachStdin: true,
+        AttachStdout: true,
+        AttachStderr: true,
+        Tty: true,
+        Cmd: ["sh", "-c", "tail -f /dev/null"],
+        OpenStdin: true,
+      })
+      .then(function (container) {
+        containerInfo[socketId] = { container, id: container.id };
+        container.start(async function (data) {
+          console.log("data", data);
+          const exec = await container.exec({
+            Cmd: ["/bin/sh"],
+            AttachStderr: true,
+            AttachStdout: true,
+            AttachStdin: true,
+            Tty: true,
+          });
+          exec.start({ hijack: true, stdin: true }, function (err, stream) {
+            if (err) {
+              console.log("err", err);
+              return;
+            }
+            stream.on("data", (chunk) => {
+              console.log("chunk", chunk);
+              socket.emit("data", chunk);
             });
-            exec.start({ hijack: true, stdin: true }, function (err, stream) {
-              if (err) {
-                console.log("err", err);
-                return;
-              }
-              stream.on("data",(chunk)=>{
-                console.log("chunk", chunk);
-                socket.emit("data", chunk);
-              })
-              socket.on("data", (data) => {
-                console.log("data",data)
-                stream.write(data);
-              });
+            socket.on("data", (data) => {
+              console.log("data received", data);
+              stream.write(data);
             });
           });
         });
-      console.log(`container started`);
-    } catch (error) {
-      console.log("error",error);
-    }
-}
+      });
+    console.log(`container started`);
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 
 io.on("connection", (socket) => {
-    // console.log(socket.id);
-    socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
-        console.log(roomId, username);
-        userSocketMap[socket.id] = username;
-        socket.join(roomId);
-        const clients = getAllConnectedClients(roomId);
-        console.log({ clients });
-        clients.forEach(({ socketId }) => {
-            io.to(socketId).emit(ACTIONS.JOINED, {
-                clients,
-                username,
-                socketId: socket.id,
-            });
-        });
-        spinContainer(socket, socket.id);
-    })
-
-    socket.on(ACTIONS.BOARD_CHANGE, ({ roomId, elements }) => {
-        // console.log({ elements });
-        socket.to(roomId).emit(ACTIONS.BOARD_CHANGE, { elements })
-    })
-
-    socket.on(ACTIONS.PERMISSION_CHANGE, ({ roomId, changedPermissionUser }) => {
-        socket.to(roomId).emit(ACTIONS.PERMISSION_CHANGE, { changedPermissionUser })
-    })
-
-    socket.on(ACTIONS.CODE_CHANGE, ({ code, roomId, language, version }) => {
-        socket.to(roomId).emit(ACTIONS.CODE_CHANGE, { code, language, version })
-    })
-
-    socket.on(ACTIONS.MESSAGE_SEND, ({ roomId, value }) => {
-        io.in(roomId).emit(ACTIONS.MESSAGE_SEND, { roomId, value });
-    })
-
-    socket.on('disconnecting', () => {
-        const rooms = [...socket.rooms];
-        rooms.forEach((roomId) => {
-            socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
-                socketId: socket.id,
-                username: userSocketMap[socket.id],
-            });
-        });
-        delete userSocketMap[socket.id];
-        socket.leave();
-        const {container,id} = containerInfo[socket.id]
-        container?.stop(id)
+  // console.log(socket.id);
+  socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
+    console.log(roomId, username);
+    userSocketMap[socket.id] = username;
+    socket.join(roomId);
+    const clients = getAllConnectedClients(roomId);
+    console.log({ clients });
+    clients.forEach(({ socketId }) => {
+      io.to(socketId).emit(ACTIONS.JOINED, {
+        clients,
+        username,
+        socketId: socket.id,
+      });
     });
-    
-})
+    spinContainer(socket, socket.id);
+  });
 
+  socket.on(ACTIONS.BOARD_CHANGE, ({ roomId, elements }) => {
+    // console.log({ elements });
+    socket.to(roomId).emit(ACTIONS.BOARD_CHANGE, { elements });
+  });
+
+  socket.on(ACTIONS.PERMISSION_CHANGE, ({ roomId, changedPermissionUser }) => {
+    socket
+      .to(roomId)
+      .emit(ACTIONS.PERMISSION_CHANGE, { changedPermissionUser });
+  });
+
+  socket.on(ACTIONS.CODE_CHANGE, ({ code, roomId, language, version }) => {
+    socket.to(roomId).emit(ACTIONS.CODE_CHANGE, { code, language, version });
+  });
+
+  socket.on(ACTIONS.MESSAGE_SEND, ({ roomId, value }) => {
+    io.in(roomId).emit(ACTIONS.MESSAGE_SEND, { roomId, value });
+  });
+
+  socket.on("disconnecting", () => {
+    const rooms = [...socket.rooms];
+    rooms.forEach((roomId) => {
+      socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
+        socketId: socket.id,
+        username: userSocketMap[socket.id],
+      });
+    });
+    delete userSocketMap[socket.id];
+    socket.leave();
+    if (containerInfo[socket.id]) {
+      const { container, id } = containerInfo[socket.id];
+      container?.stop(id);
+    }
+  });
+});
 
 server.listen(process.env.PORT, () => {
-    console.log("Listening on port " + process.env.PORT);
-})
+  console.log("Listening on port " + process.env.PORT);
+});
